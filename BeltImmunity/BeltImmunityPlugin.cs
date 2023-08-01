3af98cc4a7eb66299a07c8a1eadbd9745c68ad10
CubeSuite <cubesuite@outlook.com>
2023-08-01 12:11:29 +0100

    Initial commit

---
+++ BeltImmunity/BeltImmunityPlugin.cs
@@ -0,0 +1,30 @@
+﻿using BeltImmunity.Patches;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace BeltImmunity
+{
+    [BepInPlugin(MyGUID, PluginName, VersionString)]
+    public class BeltImmunityPlugin : BaseUnityPlugin
+    {
+        private const string MyGUID = "com.equinox.BeltImmunity";
+        private const string PluginName = "BeltImmunity";
+        private const string VersionString = "1.0.0";
+
+        private static readonly Harmony Harmony = new Harmony(MyGUID);
+        public static ManualLogSource Log = new ManualLogSource(PluginName);
+
+        private void Awake() {
+            // Apply all of our patches
+            Logger.LogInfo($"PluginName: {PluginName}, VersionString: {VersionString} is loading...");
+            Harmony.PatchAll();
+            Logger.LogInfo($"PluginName: {PluginName}, VersionString: {VersionString} is loaded.");
+            Log = Logger;
+
+            Harmony.CreateAndPatchAll(typeof(PlayerFirstPersonControllerPatch));
+        }
+    }
+}
---
+++ BeltImmunity/Patches/PlayerFirstPersonControllerPatch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+
+namespace BeltImmunity.Patches
+{
+    public class PlayerFirstPersonControllerPatch
+    {
+        [HarmonyPatch(typeof(PlayerFirstPersonController), "CalculateBeltMovement")]
+        [HarmonyPrefix]
+        static bool setRidingBeltFalse(PlayerFirstPersonController __instance) {
+            Type type = typeof(PlayerFirstPersonController);
+            FieldInfo fieldInfo = type.GetField("ridingBelt", BindingFlags.NonPublic | BindingFlags.Instance);
+            if (fieldInfo != null) {
+                fieldInfo.SetValue(__instance, false);
+            }
+            return false;
+        }
+    }
+}
